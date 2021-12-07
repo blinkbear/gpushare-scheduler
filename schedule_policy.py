a40dd840bf35478d7c30609f0ae7ac65586b6042
blinkbear <846778224@qq.com>
2021-12-07 15:26:32 +0800

    read prometheus

--- schedule_policy.py
+++ schedule_policy.py
@@ -8,85 +8,73 @@ from json import loads as json_loads
 import kubernetes as k8s
 import numpy as np
 from configs import Configs
+from prometheus_api_client import PrometheusConnect,MetricRangeDataFrame, metric_range_df
+from prometheus_api_client.utils import parse_datetime
+from datetime import timedelta
+import pandas as pd
+
 Configs = Configs()
 
 formatter = " %(asctime)s | %(levelname)-6s | %(process)d | %(threadName)-12s |" \
             " %(thread)-15d | %(name)-30s | %(filename)s:%(lineno)d | %(message)s |"
 basicConfig(level=INFO, format=formatter)
+query_metrics = [
+    'cgroup_monitor_monitored_cpu_psi', 'cgroup_monitor_monitored_io_psi',
+    'cgroup_monitor_monitored_memory_psi'
+]
 logger = getLogger("gpu_scheduler")
 
+# init prometheus api
+prom_client = PrometheusConnect(url="172.169.8.5:9090")
 
-# get avaliable nodes
+
+# get prometheus metrics
+def GetMetrics(query_metrics=query_metrics,chunck_size="10s", query_range="1d"):
+    start_time = parse_datetime(query_range)
+    end_time = parse_datetime("now")
+    chunk_size = timedelta(seconds=chunck_size)
+    result = pd.DataFrame()
+    for metric in query_metrics:
+        metric_data = prom_client.get_metric_range_data(
+            metric,  # this is the metric name and label config
+            start_time=start_time,
+            end_time=end_time,
+            chunk_size=chunk_size,
+        )
+        metric_data_df = MetricRangeDataFrame(metric_data).reset_index()
+        result.append(metric_range_df)
+    return result
+
+
+'''
+kernel scheduling function.
+1. first stage: get all scheduled pods and deploy them to the node with minimum average 60s pod some cpu psi value last day.
+'''
 def getUseNode(k8sCoreV1api, namespace):
-    # podInstance = k8sCoreV1api.list_namespaced_pod(namespace)
     podInstance = getScheduledPod(k8sCoreV1api, namespace)
     # print(podInstance)
     useNodeName = []
     newPodInstance = []
-
-    # 1. if there are free node, return these nodes to schedule
-    # nodeInstance = k8sCoreV1api.list_node(label_selector="gpushare/has-processes=false")
-    # if nodeInstance is not None:
-    #     for item in nodeInstance.items:
-    #         useNodeName.append(item.metadata.name)
-    #     return useNodeName
-
-    # 2. select nodes with one free GPUs
-    # 3. select nodes with the minimum interference
-    nodeInstance = k8sCoreV1api.list_node(label_selector="gpushare=true")
+    node_psi_metrics = GetMetrics() 
+    node_avg_metrics = node_psi_metrics[(node_psi_metrics['type']=='some')&(node_psi_metrics['window']=='60s')].groupby(['instance']).agg({'value':np.mean, 'timestamp':np.max}).reset_index()[['instance','value']]
+    node_avg_metrics.sort_values(by='value', ascending=False, inplace=True)
+    nodeInstance = k8sCoreV1api.list_node()
     for i in podInstance:
         if i.status.phase == 'Pending' and i.spec.node_name is None and i.spec.scheduler_name is not None:
-            print("wychen")
-            pod_requests = i.spec.containers[0].resources.requests
-            job_name = i.metadata.name
-
-            gpu_request, gpu_mem_request = 0, 0
-            # print(pod_requests)
-            if "gpushare/gpu-count" in pod_requests.keys():
-                gpu_request = int(pod_requests["gpushare/gpu-count"])
-            if "gpushare/gpu-mem" in pod_requests.keys():
-                gpu_mem_request = int(pod_requests["gpushare/gpu-mem"])
-            if "memory" in pod_requests.keys():
-                memory_request = int(pod_requests["memory"][:-1])
-            if "cpu" in pod_requests.keys():
-                cpu_request = int(pod_requests["cpu"])
             for j in nodeInstance.items:
-                # print(j.metadata.annotations)
-                nodeInfo = NodeInfo(j, 0)
-                # print(nodeInfo)
-                if j.status.conditions[-1].status == "True" and j.status.conditions[-1].type == "Ready":
-                    envs = client.V1EnvVar(
-                        name='CUDA_VISIBLE_DEVICES', value="1")
-                    # print(i.spec.containers[0])
-                    i.spec.containers[0].env = envs
-                        # memory = str(j.status.capacity['memory'])[:-2]
-                        # memory = float(int(memory)/1024/1024)
-                        # cpu = int(j.status.capacity["cpu"])
-                        # gpu_count = nodeInfo.gpuCount
-                        # print(nodeInfo.processes)
-                        # print(nodeInfo.memoryCapacity)
-                        # print(nodeInfo.memoryUsed)
-                        # print(nodeInfo.gpuUtil)
-                        # print(nodeInfo.memUtil)
-                        # # print(nodeInfo.getDeviceInfo(j, 0))
-                        # if gpu_count > 0:
-                        #     for k in range(gpu_count):
-                        #        deviceInfo = nodeInfo.getDeviceInfo(j, k)
-                        #        if deviceInfo.gpu_util < Configs.GPU_UTIL_THRESHOLD and deviceInfo.mem_util < Configs.MEM_UTIL_THRESHOLD and cpu >= cpu_request and memory >= memory_request and gpu_mem_request < deviceInfo.mem_capacity - deviceInfo.mem_used:
-                        #             process = deviceInfo.processes
-                        #             interference_score = Interference.getInterferenceScore(process, job_name)
-                        #             if interference_score < Configs.INTERFERENCE_THRESHOLD:
-                        #                 print(j.metadata.name)
-                        #                 print(k)
-                        #                 i.spec.containers[0].env['CUDA_VISIBLE_DEVICES'] = k
-                        #                 useNodeName.append(j.metadata.name)
-                    useNodeName.append(j.metadata.name)
-            # newPodInstance(i)
+                
     print(useNodeName)
     return useNodeName
 
-# get pods that need be scheduled
 
+# get node available cpu and memory
+def getFreeResource(k8sCoreV1api, nodeName):
+    nodeInfo = k8sCoreV1api.read_node(nodeName)
+    nodeInfo = NodeInfo(nodeInfo)
+    return nodeInfo.getFreeResource()
+
+
+# get pods that need be scheduled
 
 def getScheduledPod(k8sCoreV1api, namespace):
     podInstance = k8sCoreV1api.list_namespaced_pod(namespace)
@@ -98,6 +86,7 @@ def getScheduledPod(k8sCoreV1api, namespace):
     print(len(scheduledPods))
     print(scheduledPods)
     return scheduledPods
+
 
 # binding pod on some node
 
@@ -119,8 +108,8 @@ def podBinding(k8sCoreV1api, pod, nodeName, namespace):
 
     #
     try:
-        logger.info("Binding Pod: %s  to  Node: %s",
-                    pod.metadata.name, nodeName)
+        logger.info("Binding Pod: %s  to  Node: %s", pod.metadata.name,
+                    nodeName)
         k8sCoreV1api.create_namespaced_binding(namespace, body)
         return True
     except Exception as e:
@@ -143,10 +132,6 @@ def podScheduling(k8sCoreV1api, useNodeName, scheduledPods, namespace):
     # nodeName = useNodeName
     print("select node: ", nodeName)
     for pod in scheduledPods:
-        envs = client.V1EnvVar(name='CUDA_VISIBLE_DEVICES', value="1")
-        # print(i.spec.containers[0])
-        pod.spec.containers[0].env = envs
-        
         print("start schedule..." + str(pod.metadata.name))
         re = podBinding(k8sCoreV1api, pod, nodeName, namespace)
         if re:
@@ -160,24 +145,34 @@ def watchPodEvents(k8sCoreV1api, namespace):
             logger.info("Check pod event...")
             try:
                 watcher = watch.Watch()
-                for event in watcher.stream(k8sCoreV1api.list_namespaced_pod, namespace=namespace, timeout_seconds=20):
-                    logger.info(f"Event: {event['type']} {event['object'].kind}, {event['object'].metadata.namespace}, {event['object'].metadata.name}, {event['object'].status.phase}")
+                for event in watcher.stream(k8sCoreV1api.list_namespaced_pod,
+                                            namespace=namespace,
+                                            timeout_seconds=20):
+                    logger.info(
+                        f"Event: {event['type']} {event['object'].kind}, {event['object'].metadata.namespace}, {event['object'].metadata.name}, {event['object'].status.phase}"
+                    )
                     if event["object"].status.phase == "Pending":
                         try:
-                            logger.info(f'{event["object"].metadata.name} needs scheduling...')
+                            logger.info(
+                                f'{event["object"].metadata.name} needs scheduling...'
+                            )
                             pod_name = event["object"].metadata.name
-                            logger.info("Processing for Pod: %s/%s",
-                                        namespace, pod_name)
+                            logger.info("Processing for Pod: %s/%s", namespace,
+                                        pod_name)
                             node_name = getUseNode(k8sCoreV1api, namespace)
                             if node_name:
-                                logger.info("Namespace %s, PodName %s , Node Name: %s",
-                                            namespace, pod_name, node_name)
-                                scheduledPods = getScheduledPod(k8sCoreV1api, namespace)            
-                                res = podScheduling(
-                                    k8sCoreV1api, node_name, scheduledPods, namespace)
+                                logger.info(
+                                    "Namespace %s, PodName %s , Node Name: %s",
+                                    namespace, pod_name, node_name)
+                                scheduledPods = getScheduledPod(
+                                    k8sCoreV1api, namespace)
+                                res = podScheduling(k8sCoreV1api, node_name,
+                                                    scheduledPods, namespace)
                                 logger.info("Response %s ", res)
                             else:
-                                logger.error(f"Found no valid node to schedule {pod_name} in {namespace}")
+                                logger.error(
+                                    f"Found no valid node to schedule {pod_name} in {namespace}"
+                                )
                         except ApiException as e:
                             logger.error(json_loads(e.body)["message"])
                         except ValueError as e:
@@ -192,7 +187,8 @@ def watchPodEvents(k8sCoreV1api, namespace):
         except:
             logger.exception("Ignoring Exception & listening for pod events")
 
-if __name__ == '__main__':                                                         
+
+if __name__ == '__main__':
     namespace = "gpu-share"
     scheduler_name = "gpu_scheduler"
     # 加载配置文件 这里的配置文件是集群中的.kube/config 文件，直接去集群中粘贴过来即可
